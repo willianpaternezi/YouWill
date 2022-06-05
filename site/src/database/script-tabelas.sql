b7233f76053f6d102829105798fcf4e8416b9152
willpaternezi <willian.paternezi@sptech.school>
2022-06-05 16:35:04 -0300

    dashboard

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,7 +1,3 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-/* para workbench - local - desenvolvimento */
 CREATE DATABASE YouWill;
 
 USE YouWill;
@@ -22,30 +18,18 @@ CREATE TABLE aviso (
 );
 
 create table votacao (
-/* em nossa regra de negócio, votacao cade usuario so pode realizar um voto */
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+    voto_sydney INT,
+    voto_brisbane INT,
+    voto_canberra INT,
+    fk_usuario INT, 
+	FOREIGN KEY(fk_usuario) REFERENCES usuario(id)
 );
 
+insert into usuario()
+values (null, 'a' , 'a@gmail.com', 'teste1'),
+(null, 'b', 'b@gmail.com', 'teste1');
 
-
-/* para sql server - remoto - produção */
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table votacao (
-/* em nossa regra de negócio, um votacao tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
+insert into votacao ()
+values (null,1, 0 , 0 , 1),
+(null, 0, 1 , 0, 2);
